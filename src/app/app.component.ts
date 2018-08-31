5aae7c1281b27008ff1b4b19d0a21f590a4b3a31
Leo <bboy_sky94@hotmail.com>
2018-09-01 03:18:55 +0800

    Chart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
+
 
 @Component({
   selector: 'app-root',
@@ -6,6 +8,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
+LineChart=[];
+  DoughnutChart=[];
+	ngOnInit(){
+		// Line chart:
+		this.LineChart = new Chart('lineChart', {
+		  type: 'line',
+		data: {
+		 labels: ["Jan", "Feb", "March", "April", "May", "June","July","Aug","Sep","Oct","Nov","Dec"],
+		 datasets: [{
+		     label: 'Number of Items Sold in Months',
+		     data: [9,7 , 3, 5, 2, 10,15,16,19,3,1,9],
+		     fill:false,
+		     lineTension:0.2,
+		     borderColor:"red",
+		     borderWidth: 1
+		 }]
+		}, 
+		options: {
+		 title:{
+		     text:"Line Chart",
+		     display:true
+		 },
+		 scales: {
+		     yAxes: [{
+		         ticks: {
+		             beginAtZero:true
+		         }
+		     }]
+		 }
+		}
+		});
 
+		this.DoughnutChart = new Chart('SkillsChart', {
+			type: 'doughnut',
+			data: {
+				datasets: [{
+					data: [10,30,60,20],
+					backgroundColor: ["#F00","#026df3","#3fff45","#F90"],
+					borderColor: ["transparent","transparent","transparent","transparent"],
+					borderWidth: [0,0,0,0]
+				}],
+				labels: [
+					'Angular',
+					'JQuery',
+					'PHP',
+					'CSS'
+				]
+			};
+
+		});
+	}
 }
